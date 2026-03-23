ce9f6f0e5b9fe76afa8b2d73630e3fa15e2e6feb
Jonathan240105 <jonathan.quito@live.u-tad.com>
2026-03-23 16:53:56 +0100

    Configuración y preparación de navegación entre pantallas

--- app/src/main/java/com/example/pokenexusapplication/Controller.kt
+++ app/src/main/java/com/example/pokenexusapplication/Controller.kt
@@ -1,2 +1,39 @@
 package com.example.pokenexusapplication
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pokenexusapplication.Views.Screens.PantallaCarga
+import com.example.pokenexusapplication.Views.Screens.PantallaDetallada
+import com.example.pokenexusapplication.Views.Screens.PantallaEvoluciones
+import com.example.pokenexusapplication.Views.Screens.PantallaInicial
+import com.example.pokenexusapplication.Views.Screens.PantallaMovimientos
+import com.example.pokenexusapplication.Views.Screens.PantallaPrincipal
+
+@Composable
+fun Controller() {
+    val navController = rememberNavController()
+
+    NavHost(navController, Rutas.PANTALLA_INICIAL) {
+
+        composable(Rutas.PANTALLA_INICIAL) {
+            PantallaInicial()
+        }
+        composable(Rutas.PANTALLA_CARGA) {
+            PantallaCarga()
+        }
+        composable(Rutas.PANTALLA_PRINCIPAL) {
+            PantallaPrincipal()
+        }
+        composable(Rutas.PANTALLA_DETALLADA) {
+            PantallaDetallada()
+        }
+        composable(Rutas.PANTALLA_EVOLUCIONES) {
+            PantallaEvoluciones()
+        }
+        composable(Rutas.PANTALLA_MOVIMIENTOS) {
+            PantallaMovimientos()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pokenexusapplication/Rutas.kt
@@ -0,0 +1,10 @@
+package com.example.pokenexusapplication
+
+object Rutas {
+    const val PANTALLA_INICIAL = "pantallaInicial"
+    const val PANTALLA_CARGA = "pantallaCarga"
+    const val PANTALLA_PRINCIPAL = "pantallaPrincipal"
+    const val PANTALLA_DETALLADA = "pantallaDetallada"
+    const val PANTALLA_EVOLUCIONES = "pantallaEvoluciones"
+    const val PANTALLA_MOVIMIENTOS = "pantallaMovimientos"
+}
